395e9aa04b2d02420612eae5ff5e369d5869b979
Griffin Leclerc <leclercgriffin@gmail.com>
2019-12-12 10:09:54 -0500

    add some error checking and re-add precipitation analysis

--- weatherSender.py
+++ weatherSender.py
@@ -5,6 +5,7 @@ import smtplib
 
 # Location you want the weather from
 location = "Durham_NH"
+url = "http://wttr.in/" + location + "?format=j1"
 
 # address you want the message sent from
 # to protect those using this script, email address and password are gathered from environment variables
@@ -22,7 +23,10 @@ port = 587
 IndexToNiceTime = {0: " in the morning", 1: " at noon", 2: " in the evening", 3: " at night"}
 
 # Get the weather data
-response = requests.get("http://wttr.in/" + location + "?format=j1")
+response = requests.get(url)
+
+if response.status_code != 200:
+    print("Unable to reach " + url + ". Error code: " + response.status_code)
 
 data = response.json()
 
@@ -62,29 +66,33 @@ condition + "\n" + \
 "High of " + str(highTempF) + highTime + "\n" + \
 "Low of " + str(lowTempF) + lowTime
 
-# Currently, wttr.in has been reporting precipitation percentages incorrectly
-# so this code is not used
+# Daily precipitation chances
 
-# rainChance = 0
-# snowChance = 0
+rainChance = 0
+snowChance = 0
 
-# for i in range (4):
-#     rain = int(data["weather"][0]["hourly"][i]["chanceofrain"])
-#     snow = int(data["weather"][0]["hourly"][i]["chanceofsnow"])
+for i in range (4):
+    rain = int(data["weather"][0]["hourly"][i]["chanceofrain"])
+    snow = int(data["weather"][0]["hourly"][i]["chanceofsnow"])
 
-#     if rain > snowChance:
-#         precipChance = rain
-#     if snow > snowChance:
-#         snowChance = snow
+    if rain > snowChance:
+        rainChance = rain
+    if snow > snowChance:
+        snowChance = snow
 
-# if rainChance > 0:
-#     message += "\n" + str(rainChance) + "% chance of rain"
-# if snowChance > 0:
-#     message += "\n" + str(snowChance) + "% chance of snow"
+if rainChance > 0:
+    message += "\n" + str(rainChance) + "% chance of rain"
+if snowChance > 0:
+    message += "\n" + str(snowChance) + "% chance of snow"
 
 
 with smtplib.SMTP(mailServer, port) as smtp:
-    smtp.starttls()
-    smtp.login(email_address, email_password)
+    try:
+        smtp.starttls()
+        smtp.login(email_address, email_password)
 
-    smtp.sendmail(email_address, recipient, message)
+        smtp.sendmail(email_address, recipient, message)
+    except Exception as e:
+        print("Unable to send message: " + str(e))
+        print("Please ensure your credentials are correct. Such as desired email server and recipient address.")
+        print("Also ensure you have system environment variables EMAIL_ADDRESS and EMAIL_PASS set accordingly.")
