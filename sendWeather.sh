931afc2a334e1d4b5bca272ddf541be28d7f151d
Griffin Leclerc <leclercgriffin@gmail.com>
2019-12-18 17:54:42 -0500

    slight restructure to ensure users run the bash script and not just the python file

--- sendWeather.sh
+++ sendWeather.sh
@@ -1,12 +1,12 @@
 CONFIG="config.ini"
 
 if ! [ -f "$CONFIG" ]; then
-    python3 generateConfig.py
+    python3 src/generateConfig.py
     echo "A configutation file has been generated in this folder."
     echo "Please take a moment to fill it out."
     echo "Otherwise the message will not be sent properly and/or may contain irrelivant information."
     exit 0
 else
-    python3 weatherSender.py
+    python3 src/weatherSender.py
 fi
 
